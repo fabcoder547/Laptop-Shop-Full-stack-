71049b2003246f3eb9e36dd72fe76e77e23f9761
fabcoder547 <atharvjoshi8149@gmail.com>
2020-03-30 01:13:06 +0530

    done with authentication

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,11 @@
 const User = require('../models/user')
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
 
-const { check, validationResult } = require('express-validator');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
 
 exports.signup = (req, res) => {
@@ -8,14 +13,80 @@ exports.signup = (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors })
+        return res.status(422).json({
+            errors: errors
+        })
     }
     const user = new User(req.body);
     user.save((err, user) => {
         if (err) {
-            return res.status(400).json({ msg: "User is Not able to save in a database" })
+            return res.status(400).json({
+                msg: "User is Not able to save in a database"
+            })
         }
-        res.json({ name: user.name, lastname: user.lastname, email: user.email })
+        res.json({
+            name: user.name,
+            lastname: user.lastname,
+            email: user.email
+        })
+    })
+
+
+}
+
+
+exports.signin = (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors
+        })
+    }
+    User.findOne({
+        email: req.body.email
+    }, (err, user) => {
+        if (err) {
+            return res.status(400).json({
+                msg: "error in finding a user"
+            })
+        }
+
+        if (!user) {
+            return res.status(400).json({
+                msg: "User with this email is not exists"
+            })
+        }
+        if (!user.authenticate(req.body.password)) {
+            return res.json({
+                msg: "Email and password doesnt match"
+            });
+        }
+        var token = jwt.sign({
+            _id: user._id
+        }, process.env.SECRET);
+        res.cookie("token", token, {
+                expire: '1h'
+            })
+            //send response
+        console.log(res.cookie())
+
+        const {
+            _id,
+            name,
+            email,
+            role
+        } = user;
+
+        return res.json({
+            token,
+            user: {
+                _id,
+                name,
+                email,
+                role
+            }
+        });
+
     })
 
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { check } = require('express-validator');
-const { signup, signout } = require('../controllers/auth')
+const {
+    check
+} = require('express-validator');
+const {
+    signup,
+    signout,
+    signin
+} = require('../controllers/auth')
 
-
+// SIGNUP :: POST :: PUBLIC
 router.post('/signup', [
-    check('name').isLength({ min: 3 }).withMessage("name should be atleast of 3 characters"),
+    check('name').isLength({
+        min: 3
+    }).withMessage("name should be atleast of 3 characters"),
     check("email").isEmail().withMessage("email is required"),
-    check("password").isLength({ min: 3 }).withMessage("Password should be of atleast 3 charaters")
+    check("password").isLength({
+        min: 3
+    }).withMessage("Password should be of atleast 3 charaters")
 ], signup)
 
+// SIGNIN ::POST ::PUBLIC
+router.post('/signin', [
+    check("email").isEmail().withMessage("EMAIL IS REQUIRED"),
+    check("password").isLength(1).withMessage("Password is required"),
+], signin)
 
 router.get('/signout', signout)
 
