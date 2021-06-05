1016199d4b91c0b64d2c3fa23b115fb5d371e293
fabcoder547 <atharvjoshi8149@gmail.com>
2021-06-05 21:10:50 +0530

    footer changed

--- client/src/core/Base.js
+++ client/src/core/Base.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Base = ({
@@ -22,11 +23,55 @@ const Base = ({
       <div className="container my-5 text-center main-content">{children}</div>
 
       <div className="myfooter">
-        <p className="footer-title">Laptop shop</p>
-        <p className="footer-desc">
-          If You have any questions feel free to ask{" "}
+        <p className="footer-title">
+          Hello,I am Atharv Joshi. This is my peronal project
         </p>
-        <button className="btn btn-success btn -lg">Contact Us</button>
+        <p className="footer-desc">
+          If You have any questions feel free to ask. View my profile below.
+        </p>
+        <div
+          style={{
+            display: "flex",
+
+            width: "90%",
+            margin: "auto",
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <a
+            target="blank"
+            href="https://www.linkedin.com/in/atharvjoshi8149"
+            className="btn btn-primary btn-lg"
+            style={{
+              background: "blue",
+              padding: "7px 60px",
+              borderRadius: "5px",
+              border: "none",
+              marginLeft: "0",
+            }}
+          >
+            <h6 style={{ fontSize: "25px" }}>
+              <i class="fa fa-linkedin"></i> Linkedin
+            </h6>
+          </a>
+          <a
+            target="blank"
+            href="https://github.com/fabcoder547/"
+            className="btn btn-primary btn-lg"
+            style={{
+              background: "brown",
+              padding: "7px 60px",
+              borderRadius: "5px",
+              border: "none",
+              marginRight: "0",
+            }}
+          >
+            <h6 style={{ fontSize: "25px" }}>
+              <i class="fa fa-github"></i> Github
+            </h6>
+          </a>
+        </div>
       </div>
     </div>
   );
